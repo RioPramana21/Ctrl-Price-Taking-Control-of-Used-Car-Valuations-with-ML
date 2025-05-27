332daf058b08b600e534372adf563c8f7e6d3d0b
RioPramana21 <rio.pramana@binus.ac.id>
2025-05-28 05:47:13 +0700

    Finalized feature engineering

--- code_files/custom_pipeline_utils.py
+++ code_files/custom_pipeline_utils.py
@@ -1,9 +1,25 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 
+class DropColumns(BaseEstimator, TransformerMixin):
+    """Transformer to drop a fixed list of columns."""
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        # nothing to learn
+        return self
+
+    def transform(self, X):
+        # we expect X to be a DataFrame
+        return X.drop(columns=self.columns, errors="ignore")
+
 CURRENT_YEAR = 2022
 
 class FeatureEngineer(BaseEstimator, TransformerMixin):
-    def __init__(self, current_year=CURRENT_YEAR):
+    def __init__(self,
+                 current_year=CURRENT_YEAR,
+                 vintage_cutoff=30,
+                 big_engine_cutoff=7.0):
         self.current_year = current_year
 
     def fit(self, X, y=None):
@@ -18,10 +34,10 @@ class FeatureEngineer(BaseEstimator, TransformerMixin):
         df["Car_Age"] = self.current_year - df["Year"]
 
         # 2) Vintage flag
-        df["IsVintage"] = (df["Car_Age"] >= 30)
+        df["IsVintage"] = (df["Car_Age"] >= self.vintage_cutoff)
 
         # 3) Big engine flag ( >7L )
-        df["IsBigEngine"] = (df["Engine_Size"] > 7.0)
+        df["IsBigEngine"] = (df["Engine_Size"] > self.big_engine_cutoff)
 
         # 4) drop unused
         df = df.drop(columns=["Negotiable", "Mileage_per_Year", 
